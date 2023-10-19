9914b88282bfdc18c1fe401630448c21c5abafac
Samuel Osei Adu <asamuel355@yahoo.com>
2023-10-19 10:44:30 +0000

    update

--- admin/pages/api/auth/[...nextauth].js
+++ admin/pages/api/auth/[...nextauth].js
@@ -1,18 +1,20 @@
-import NextAuth from "next-auth"
-import GoogleProvider from 'next-auth/providers/google'
-import {MongoDBAdapter} from '@next-auth/mongodb-adapter'
-import clientPromise from "@/lib/mongodb"
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
 
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
+
     // ...add more providers here
   ],
   adapter: MongoDBAdapter(clientPromise),
-}
+  secret: process.env.NEXTJS_AUTH_SECRET_SECRET,
+};
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
--- admin/pages/components/Layout.js
+++ admin/pages/components/Layout.js
@@ -4,11 +4,12 @@ import Nav from './Nav';
 export default function Layout({children}) {
   const {data: session} = useSession()
 
+  console.log(session)
   if(!session){
     return (
       <div className="bg-blue-900 w-screen h-screen flex items-center">
         <div className="text-center w-full">
-          <button onClick={() => signIn('google')} className="bg-white p-2 text-black rounded-lg px-4">Login with Google</button>
+          <button onClick={() => signIn()} className="bg-white p-2 text-black rounded-lg px-4">Login with Google</button>
         </div>
       </div>
     )
@@ -21,7 +22,7 @@ export default function Layout({children}) {
         <div className="mt-3 mx-5">
           <div className="flex justify-between items-center">
             <h4 className="text-black ">Welcome, <span className="text-blue-400 text-xl">{ session?.user?.name}</span> </h4>
-            <img className="w-8 h-8 rounded-full" src={session?.user?.image} alt="User" />
+            <img className="w-8 h-8 rounded-full" src={session.user.image} alt="User" />
           </div>
           <hr className="my-2" />
           {children}
